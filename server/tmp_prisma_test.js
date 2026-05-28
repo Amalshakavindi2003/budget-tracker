78f440fe57d948dfd8661c860ab947ba5fa9f2f7
Ella Deniyage Kavindi <edamalsha2003@gmail.com>
2026-05-28 10:40:48 +0530

    chore: add prisma connectivity test script

---
+++ server/tmp_prisma_test.js
@@ -0,0 +1,14 @@
+﻿const {PrismaClient} = require('@prisma/client');
+const p = new PrismaClient();
+(async ()=>{
+  try{
+    await p.$connect();
+    const c = await p.user.count();
+    console.log('CONNECTED users=', c);
+  } catch(e){
+    console.error('CLIENT_ERROR:', e);
+    process.exitCode = 1;
+  } finally{
+    await p.$disconnect();
+  }
+})();
