f69b2c4a50f9a1329550bd52241ee836d361bbaa
Ella Deniyage Kavindi <edamalsha2003@gmail.com>
2026-05-31 12:31:22 +0530

    fix cors allow all

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,10 @@ import transactionRoutes from "./routes/transaction.routes";
 const app = express();
 const port = Number(process.env.PORT) || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: false
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
